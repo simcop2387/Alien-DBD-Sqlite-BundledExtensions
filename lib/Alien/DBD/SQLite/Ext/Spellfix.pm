19f8b8ccd08665bd414806b7e6346da4a75e0fce
Ryan Voots <simcop2387@simcop2387.info>
2016-12-23 05:11:09 -0800

    Start of a proper Alien:: module

---
+++ lib/Alien/DBD/SQLite/Ext/Spellfix.pm
@@ -0,0 +1,9 @@
+package Alien::DBD::SQLite::Ext::Spellfix;
+
+our $VERSION=0.0001;
+
+1;
+__END__
+=head1 NAME
+
+Alien::DBD::SQLite::Ext::Spellfix - builds the Spellfix1 extension for SQLite3 using the headers from DBD::SQLite to ensure compatibility.

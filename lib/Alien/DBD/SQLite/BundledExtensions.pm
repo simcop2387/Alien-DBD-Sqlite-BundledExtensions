bd36e88dde7e0bdc212c5877d4f31659359e4b66
Ryan Voots <simcop2387@simcop2387.info>
2016-12-29 11:08:58 -0800

    Update platform support for mswin32 and darwin.  Also include substantially more documentation on each extension provided.

--- lib/Alien/DBD/SQLite/BundledExtensions.pm
+++ lib/Alien/DBD/SQLite/BundledExtensions.pm
@@ -1,6 +1,34 @@
 package Alien::DBD::SQLite::BundledExtensions;
 
-our $VERSION=0.0001;
+use File::ShareDir 'dist_dir';
+
+our $VERSION=0.001;
+our @extensions = qw/spellfix csv ieee754 nextchar percentile series totype wholenumber eval/;
+
+my $dbd_dist = dist_dir('DBD-SQLite');
+my $cc = $Config{cc};
+
+# TODO this probably doesn't work on windows anyway.  Need to look into how to make that work correctly anyway.
+my $lib_ext = $^O =~ /mswin/ ? 'dll' : 
+              $^O =~ /darwin/ ? 'dylib' : 'so';
+
+# TODO this needs to support mswin32
+
+
+
+sub get_build_commands {
+    my $shared = $^O =~ /darwin/ ? '-dynamiclib' : '-shared';
+    my @build_commands = map {"$cc $shared -I$dbd_dist -O2 -fPIC -o $_.$lib_ext ext/misc/$_.c"} @extensions;
+
+    return @build_commands;
+}
+
+sub get_install_commands {
+    my $copy_cmd = $^O =~ /mswin/ ? 'copy' : 'cp';
+    my @install_commands = map {"$copy_cmd $_.$lib_ext %s"} @extensions;
+
+    return @install_commands;
+}
 
 1;
 __END__
@@ -10,28 +38,3 @@ Alien::DBD::SQLite::BundledExtesions - builds a series of SQLite extensions prov
 
 See L<DBD::SQLite::BundledExtensions> for more information about the extensions
 
-It provides the following extensions from the SQLite source:
-
-=over
-
-=item csv
-
-=item eval
-
-=item ieee754 
-
-=item nextchar 
-
-=item percentile 
-
-=item series 
-
-=item spellfix 
-
-=item totype 
-
-=item wholenumber 
-
-=back
-
-=head1
--- t/01-load-all.t
+++ t/01-load-all.t
@@ -6,22 +6,18 @@ use Data::Dumper;
 use DBI;
 use File::Find;
 use DBD::SQLite::BundledExtensions;
+use Alien::DBD::SQLite::BundledExtensions;
 
-use Test::Simple tests => 9;
+use Test::Simple tests => 10;
 
-#ok(defined $dbh, "Can create SQLite in memory DB");
+my $dbh=DBI->connect('dbi:SQLite:dbname=:memory:',
+    "",
+    "",
+    { RaiseError => 1, PrintError => 0 }
+);
 
-for (qw/spellfix csv ieee754 nextchar percentile series totype wholenumber eval/) {
-    # Can't seem to load more than one extension at a time.  I'm not sure why.
-    my $dbh=DBI->connect('dbi:SQLite:dbname=:memory:',
-        "",
-        "",
-        { RaiseError => 1, PrintError => 0 }
-    );
-    use Data::Dumper;
+ok(defined $dbh, "Can create SQLite in memory DB");
+
+for (@Alien::DBD::SQLite::BundledExtensions::extensions) {
     ok(DBD::SQLite::BundledExtensions->_load_extension($dbh, $_), "Load extension $_");
 }
-
-#my (@a) = $dbh->selectrow_array('SELECT spellfix1_editdist("PLYMRY", "PLYMR");');
-
-#print Dumper(\@a);
